6d91c9151187beec5e7dfb5fd5fce4b1cf6b5e18
chloejliu <chloe.liu.wa@gmail.com>
2026-06-07 21:53:34 -0700

    Create 455assign-cookies.cpp

---
+++ 455assign-cookies.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        int ans = 0;
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+        for (int i = 0, j = 0; i < g.size() && j < s.size(); j++) {
+            if (s[j] >= g[i]) {
+                ans++;
+                i++;
+            }
+        }
+        return ans;
+    }
+};
