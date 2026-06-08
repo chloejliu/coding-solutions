acb4213b4c2effaedbd376423dffd56ce99c639b
chloejliu <chloe.liu.wa@gmail.com>
2026-06-07 22:32:09 -0700

    Create 326power-of-three.cpp

---
+++ 326power-of-three.cpp
@@ -0,0 +1,17 @@
+//1-1
+class Solution {
+public:
+    bool isPowerOfThree(int n) 
+    {
+        if(n==1)
+            return true;
+        if(n<=0)
+            return false;
+        for(; n>1; n/=3)
+        {
+            if(n%3!=0)
+                return false;
+        }
+        return true;
+    }
+};
