f0bd944cfb50860aa0ce652db4d325faa28b409a
chloejliu <chloe.liu.wa@gmail.com>
2026-06-07 22:28:29 -0700

    Create 367valid-perfect-square.cpp

---
+++ 367valid-perfect-square.cpp
@@ -0,0 +1,10 @@
+//1-2
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        for(long i=1; i*i<=num; i++)
+            if(i*i==num)
+                return true;
+        return false;
+    }
+};
