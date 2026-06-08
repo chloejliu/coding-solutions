d84ea113739b80ed0be88ae76079f0b9eb00ca56
chloejliu <chloe.liu.wa@gmail.com>
2026-06-07 22:29:37 -0700

    Create 1342number-of-steps-to-reduce-a-number-to-zero.cpp

---
+++ 1342number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -0,0 +1,14 @@
+//1-1
+class Solution {
+public:
+    int numberOfSteps(int num) 
+    {
+        int i=0;
+        for(; num>0; i++)
+            if(num%2==0)
+                num/=2;
+            else
+                num--;
+        return i;
+    }
+};
